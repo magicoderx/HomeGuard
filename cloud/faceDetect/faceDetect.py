f6909b86590e03446ba5c412898b2e4f2a5155ae
DevelopMagic <pesole99@live.it>
2024-09-21 16:11:24 +0200

    Solved send mail error

--- cloud/faceDetect/faceDetect.py
+++ cloud/faceDetect/faceDetect.py
@@ -5,15 +5,14 @@ import tempfile
 import vertexai
 from vertexai.generative_models import GenerativeModel, Part, Image
 import smtplib
+from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 app = Flask(__name__)
-
-# Load key for Service Account
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = './credentials.json'
+err_str = "Specified environment variable is not set."
 
 # Init generative model in Europe West3 region
-vertexai.init(project=os.getenv("GSTORAGE_ID"), location="europe-west3")
+vertexai.init(project=os.environ.get("GSTORAGE_ID", err_str), location="europe-west3")
 model = GenerativeModel(model_name="gemini-1.0-pro-vision-001")
 
 @app.route('/', methods=['POST'])
@@ -38,46 +37,71 @@ def analyze_photo(file):
     ai = model.generate_content(
     [
         Part.from_image(Image.load_from_file(file)),
-        "Quanti volti vedi nell'immagine? Rispondimi con un numero intero",
+        "Quanti volti vedi nell'immagine? Rispondimi con un numero intero e senza punteggiature",
     ]
     )
     try:
         # Send email only if there are 2 or more faces
         if int(ai.text)>=2:
-            link='https://storage.cloud.google.com/'+os.getenv("GSTORAGE_ID")+'/'+file
+            link='https://storage.cloud.google.com/'+os.environ.get("GSTORAGE_ID", err_str)+'/'+os.path.basename(file)+'.jpg'
             send_email(int(ai.text),link)
-    except:
-        print("Error sending email")
+    except Exception as error:
+        # handle the exception
+        print("Error sending email:", error)
 
 def upload_to_gcs(file_path):
     client = storage.Client()
-    bucket = client.bucket(os.getenv("GSTORAGE_ID"))
-    blob = bucket.blob(os.path.basename(file_path))
+    bucket = client.bucket(os.environ.get("GSTORAGE_ID", err_str))
+    blob = bucket.blob(os.path.basename(file_path)+".jpg")
     blob.upload_from_filename(file_path)
     print(f'File {file_path} uploaded on Google Cloud Storage')
 
 def send_email(faces,photourl):
     # SMTP variables
-    smtp_user = os.getenv("SMTP_LOGIN_USER")
-    receiver_email = os.getenv("SMTP_TO")
-    password = os.getenv("SMTP_LOGIN_PASS")
+    smtp_user = os.environ.get("SMTP_LOGIN_USER", err_str)
+    receiver_email = os.environ.get("SMTP_TO", err_str)
+    password = os.environ.get("SMTP_LOGIN_PASS", err_str)
 
-    # Generate headers
-    msg = MIMEText(f"Attenzione! Sono stati rilevati ",faces," volti in casa! Guarda la foto: {photourl}")
-    msg["Subject"] = "Videosorveglianza: ATTENZIONE"
-    msg["From"] = os.getenv("SMTP_FROM")
-    msg["To"] = receiver_email
+    # Compose message
+    message = MIMEMultipart("alternative")
+    message['Subject'] = "TEST"
+    message['From'] = smtp_user
+    message['To'] = receiver_email
 
-    server = smtplib.SMTP_SSL(os.getenv("SMTP_SERVER"), os.getenv("SMTP_PORT"))
-    server.login(smtp_user, password)
-    try:
-        # Send the Email
-        server.sendmail(smtp_user, receiver_email, msg.as_string())
-        server.quit()
-        print("Email sent")
-    except:
-        server.quit()
-        print("Error: email was not sent")
+    # HTML to send
+    html = """
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Mail Content</title>
+        <style>
+            body{
+                font-family: Arial, Helvetica, sans-serif;
+            }
+        </style>
+    </head>
+    <body>
+        <p>Attenzione! Sono stati rilevati """
+    # Append variables to html
+    html=html+str(faces)
+    html=html+" volti in casa! Guarda la foto: <a href="
+    html=html+photourl
+    html=html+">qui</a></p></body></html>"
+    
+    html_part = MIMEText(html, 'html')
+    message.attach(html_part)
+    
+    # Send email
+    smtp = smtplib.SMTP(os.environ.get("SMTP_SERVER", err_str), os.environ.get("SMTP_PORT", err_str))
+    
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.login(smtp_user, password)
+    smtp.sendmail(smtp_user, receiver_email, message.as_string())
+    smtp.quit()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
