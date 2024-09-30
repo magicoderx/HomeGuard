c8b7b6610014409cc710816bddadbad591d543f3
DevelopMagic <pesole99@live.it>
2024-09-30 14:54:51 +0200

    Improve security

--- cloud/getImages/main.py
+++ cloud/getImages/main.py
@@ -1,11 +1,15 @@
 from flask import jsonify
 import os
 from google.cloud import storage
+from google.oauth2 import service_account
 import datetime
+from datetime import timedelta
 
+# Initialize service account credentials
+credentials = service_account.Credentials.from_service_account_file('./credentials.json')
 err_str = "Specified environment variable is not set."
 # Initialize google storage client
-storage_client = storage.Client()
+storage_client = storage.Client(credentials=credentials)
 
 def get_images(request):
     # Get current timestamp and parse it in firestore format
@@ -19,7 +23,7 @@ def get_images(request):
         return jsonify({'message': 'Nessuna immagine trovata per questa data.'}), 404
     
     # Get urls of images
-    image_urls = [{'name': blob.name, 'url': blob.public_url} for blob in images]
+    image_urls = [{'name': blob.name, 'url': generate_signed_url(blob)} for blob in images]
     
     return jsonify(image_urls)
 
@@ -31,8 +35,13 @@ def get_images_by_date(bucket_name, date):
 
     # Filter from date
     for blob in blobs:
-        blob_date = blob.time_created.date() 
+        blob_date = blob.time_created.date()
         if (str(blob_date) == str(date)):
             images.append(blob)
 
     return images
+
+def generate_signed_url(blob, expiration_minutes=5):
+    # Generate signed url for images
+    url = blob.generate_signed_url(expiration=timedelta(minutes=expiration_minutes))
+    return url
